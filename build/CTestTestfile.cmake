e37dba070933c62dc15ede19d97398e50a8e3c1c
CappiJoe <casper.rodsrud@gmail.com>
2021-09-08 18:14:22 +0200

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cappi/cappicar/src
+# Build directory: /home/cappi/cappicar/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rplidar_ros")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cappi/cappicar/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cappi/cappicar/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/cappi/cappicar/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cappi/cappicar/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cappi/cappicar/src:$ROS_PACKAGE_PATH"
